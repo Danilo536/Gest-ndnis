aec9838c8f25d613f8440c0917e9f49eef574b86
Danilo536 <135519407+Danilo536@users.noreply.github.com>
2024-04-05 16:54:02 +0200

    Update and rename images to script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var navbarRight = document.getElementById("navbarRight");
+    if (navbarRight.style.display === "block") {
+        navbarRight.style.display = "none";
+    } else {
+        navbarRight.style.display = "block";
+    }
+}
