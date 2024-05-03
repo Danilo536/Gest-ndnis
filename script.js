a837b23948a20e6ad4ad46fa31df0b0314e721aa
Danilo536 <135519407+Danilo536@users.noreply.github.com>
2024-05-03 20:42:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+var hamburger = document.getElementById("hamburger");
+var dropdown = document.querySelector('.dropdown');
+
+hamburger.addEventListener('click', function() {
+  dropdown.classList.toggle('active');
+});
 
