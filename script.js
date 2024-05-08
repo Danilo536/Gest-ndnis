56b2c54cfdc31f20c3a365144630008c65bac0de
Gpnde <166177483+Gpnde@users.noreply.github.com>
2024-05-08 10:05:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-var hamburger = document.getElementById("hamburger");
-var dropdown = document.querySelector('.dropdown');
-
-hamburger.addEventListener('click', function() {
-  dropdown.classList.toggle('active');
-});
 
